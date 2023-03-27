41b665be1e053c692d99bb84f9b69d81841c80bb
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2023-03-27 18:21:09 +0500

    printing payload that we got from postman

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 # request come in get methnd and url "/"
@@ -19,5 +20,6 @@ def get_data():
 
 
 @app.post("/createpost")
-def get_post():
+def get_post(payload: dict = Body(...)):
+    print(payload)
     return {"message": "post is created successfully"}
