d283389cdb918514ef7f6f4c939ed853987ffaa6
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2023-03-27 17:02:30 +0500

    just checking how FastAPI works

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+# request come in get methnd and url "/"
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "trying to create FastAPI"}
+
+# here I am going to running sample get data function
+
+
+@app.get("/data")
+def get_data():
+    return {"name": "Ishaque", "friendname": "Ahmed, Salman, Sarwan, Anshal"}
