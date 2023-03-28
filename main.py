b023523fdf38a4c26c78e9bd29f9f14e2e6477e8
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2023-03-28 12:50:10 +0500

    simple validtion is added

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
-from fastapi.params import Body
-
+from fastapi.params import Body  # bofy is used for reciveing data from frontend
+from pydantic import BaseModel  # this libaray is for setting schema
 app = FastAPI()
+
+
+# here I have created post class which inherites basemodel which help me
+#  to set schema for post request
+class Post(BaseModel):
+    title: str
+    content: str
+
 # request come in get methnd and url "/"
 
 
@@ -20,6 +28,6 @@ def get_data():
 
 
 @app.post("/createpost")
-def get_post(payload: dict = Body(...)):
-    print(payload)
-    return {"message": "post is created successfully"}
+def get_post(new_post: Post):  # we want payload has title and content
+    print(new_post.title)
+    return {"new_message": "new post"}
