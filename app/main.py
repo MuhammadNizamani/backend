8e66f4022baa41227f2108ec144ce089779e2f08
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2023-03-30 18:37:06 +0500

    update is added

--- app/main.py
+++ app/main.py
@@ -112,12 +112,12 @@ def delete_post(id: int):
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
-    index = find_index_post(id)
-    if index == None:
+    cursor.execute("""UPDATE posts SET title = %s , content = %s, 
+    published = %s WHERE id =%s  RETURNING *""", (post.title, post.content, post.published, str(id),))
+    updated_post = cursor.fetchone()
+    conn.commit()
+    if update_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id {id} does not exist")
 
-    post_dict = post.dict()
-    post_dict['id'] = id
-    my_posts[index] = post_dict
-    return {"data": post_dict}
+    return {"data": updated_post}
