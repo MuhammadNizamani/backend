ff3f2cdf72d21f53e3da3be93ee7518355f7a4de
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2023-03-31 16:24:20 +0500

    create database file applied db conncetions

---
+++ app/database.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost/fastapi"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+base = declarative_base()
