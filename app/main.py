bd95678f4555eeda1556f2ef5348d4a13e3d9e53
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2023-04-01 14:59:30 +0500

    EHN: updated the update func  using oorm

--- app/main.py
+++ app/main.py
@@ -135,13 +135,17 @@ def test_post(db: Session = Depends(get_db)):
 
 
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post):
-    cursor.execute("""UPDATE posts SET title = %s , content = %s, 
-    published = %s WHERE id =%s  RETURNING *""", (post.title, post.content, post.published, str(id),))
-    updated_post = cursor.fetchone()
-    conn.commit()
-    if updated_post == None:
+def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
+    # cursor.execute("""UPDATE posts SET title = %s , content = %s,
+    # published = %s WHERE id =%s  RETURNING *""", (post.title, post.content, post.published, str(id),))
+    # updated_post = cursor.fetchone()
+    # conn.commit()
+    post_query = db.query(model.Post).filter(model.Post.id == id)
+    post = post_query.first()
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id {id} does not exist")
+    post_query.update(updated_post.dict(), synchronize_session=False)
+    db.commit()
 
-    return {"data": updated_post}
+    return {"data": post_query.first()}
