1515243d8a41a4eee3998aba02f8b9c2f26da245
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2023-04-02 12:56:55 +0500

    EHN: get user by id func is added

--- app/main.py
+++ app/main.py
@@ -148,3 +148,12 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
 
     return new_user
+
+
+@app.get("/users/{id}", response_model=schemas.userOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with id {id} do exist")
+    return user
