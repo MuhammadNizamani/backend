0734864227728cfed8e438d49fe5755d9e94bdcd
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2023-04-02 18:32:20 +0500

    EHN: OAuth2PasswordRequestForm addeed

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter, status, HTTPException, Response, Depends
 from sqlalchemy.orm import Session
 from ..import database, schemas, models, utils, oauth2
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 router = APIRouter(tags=['Authentication'])
 
 
 @router.post('/login')
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
     user = db.query(models.User).filter(
-        models.User.email == user_credentials.email).first()
+        models.User.email == user_credentials.username).first()
 
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
