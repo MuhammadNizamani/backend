d1445c0488fc4e2ba409ecc747a55f2b281e6f6e
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2023-04-02 17:39:54 +0500

    FIX: JWT token is working fine and it tested

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,4 +1,5 @@
 from jose import JWTError, jwt
+from datetime import datetime, timedelta
 # secret key
 # Algorithm
 # expiertion time
@@ -9,3 +10,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 def create_access_token(data: dict):
     to_encode = data.copy()
+    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, status, HTTPException, Response, Depends
 from sqlalchemy.orm import Session
-from ..import database, schemas, models, utils
+from ..import database, schemas, models, utils, oauth2
 router = APIRouter(tags=['Authentication'])
 
 
@@ -15,5 +15,6 @@ def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.ge
     if not utils.varify(user_credentials.password, user.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="Invalid credentials")
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
 
-    return {"token": "Exmple token "}
+    return {"access_token": access_token, "token_type": "bearer"}
