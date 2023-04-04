b229624697e947ea5a2eed9796ea4490f8b83e22
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2023-04-04 17:05:23 +0500

    FIX: clean up the main file

--- app/database.py
+++ app/database.py
@@ -1,6 +1,10 @@
+import psycopg2
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import time
+from psycopg2.extras import RealDictCursor
+
 
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost/fastapi"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
@@ -14,3 +18,16 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+# while True:
+#     try:
+#         conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres',
+#                                 password='admin', cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print("Database connection was succesful!")
+#         break
+#     except Exception as error:
+#         print("Database fail to connect")
+#         print("error is ", error)
+#         time.sleep(3)
--- app/main.py
+++ app/main.py
@@ -1,49 +1,12 @@
-from typing import Optional, List
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-from fastapi.params import Body  # bofy is used for reciveing data from frontend
+# from fastapi.params import Body  # bofy is used for reciveing data from frontend
 # couse I can create random  ID couse I am not using DB right now
-from random import randrange
-import psycopg2
-from psycopg2.extras import RealDictCursor
-from sqlalchemy.orm import Session
-import time
+from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import user, post, auth
 
-
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
-
-
-while True:
-    try:
-        conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres',
-                                password='admin', cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connection was succesful!")
-        break
-    except Exception as error:
-        print("Database fail to connect")
-        print("error is ", error)
-        time.sleep(3)
-
-
-my_posts = [{"title": "6 sahur", "content": "I am fasting in ramzan", "id": 1},
-            {"title": "6 Iftari", "content": "I like Pakurha", "id": 2}]
-
-
-def find_post(id):
-    for p in my_posts:
-        if p['id'] == id:
-            return p
-
-
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p['id'] == id:
-            return i
-
 
 app.include_router(post.router)
 app.include_router(user.router)
