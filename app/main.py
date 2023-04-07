4b3a285acd004a9d798ae1c4264cb72ce7e301e1
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2023-04-07 18:22:12 +0500

    EHN: almost all chnages are done now api is ready

--- app/main.py
+++ app/main.py
@@ -2,11 +2,20 @@
 # couse I can create random  ID couse I am not using DB right now
 from fastapi import FastAPI
 from . import models
-from .database import engine
+# from .database import engine
 from .routers import user, post, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
 
 # models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
