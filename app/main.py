c19b690755e1fbbe03a8875a1430da27ac42b407
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2023-04-07 16:58:06 +0500

    ENH: phone numver is added

--- app/main.py
+++ app/main.py
@@ -5,10 +5,15 @@ from . import models
 from .database import engine
 from .routers import user, post, auth, vote
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+
+@app.get("/")
+def default_message():
+    return {"message": "welcome world"}
--- app/models.py
+++ app/models.py
@@ -27,6 +27,7 @@ class User(Base):
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
+    phone_number = Column(String)
 
 
 class Vote(Base):
