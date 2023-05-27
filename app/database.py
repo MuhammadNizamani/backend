79eaba07ce0199d35091b5d6ec280876e1378a73
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-05-27 13:53:37 +0500

    changing in red file

--- app/database.py
+++ app/database.py
@@ -1,8 +1,8 @@
-import psycopg2
+# import psycopg2
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from psycopg2.extras import RealDictCursor
+# from psycopg2.extras import RealDictCursor
 from .config import settings
 
 
